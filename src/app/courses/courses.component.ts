6a7b643791c23366102bc7f7b07d188ff28a7a22
samad20 <musab.aljahdhami@gmail.com>
2023-02-13 11:52:33 +0400

    adding title and use courses service

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-
+  title = 'List of courses';
+  courses;
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
 }
