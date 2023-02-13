571c34f69365d26b4660c81f725a6a5f3247c0e7
samad20 <musab.aljahdhami@gmail.com>
2023-02-13 11:52:46 +0400

    provide CoursesService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
+import { CoursesService } from './courses/courses.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CoursesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CoursesComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CoursesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
