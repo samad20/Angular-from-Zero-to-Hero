ecba6ca1f5731b1dfd4b0031db44d9c1ff48f205
samad20 <musab.aljahdhami@gmail.com>
2023-02-14 08:25:31 +0400

    adding boostrab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CoursesService } from './courses/courses.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, CoursesComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [CoursesService],
   bootstrap: [AppComponent],
 })
