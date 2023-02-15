a90f47a7f62a88c9be0ad37483e9465652be454f
samad20 <musab.aljahdhami@gmail.com>
2023-02-15 11:43:38 +0400

    aliasing Input prop

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css'],
 })
 export class FavoriteComponent implements OnInit {
-  @Input() isFavorite = false;
+  @Input('is-favorite') isFavorite = false;
 
   constructor() {}
 
