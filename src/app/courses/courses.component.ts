90b153c2b85a8c4bedb06ddbb03be63f3d7bc988
samad20 <musab.aljahdhami@gmail.com>
2023-02-21 12:53:57 +0400

    edit viewMode

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -7,7 +7,7 @@ import { CoursesService } from './courses.service';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-  viewMode = 'map';
+  viewMode = '';
   post = {
     title: '',
     isFavoriteFromCourses: true,
