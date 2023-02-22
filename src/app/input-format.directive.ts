5cf74fe2a2662cd69e9abea5c98934cb5ba685f9
samad20 <musab.aljahdhami@gmail.com>
2023-02-22 11:01:32 +0400

    add custom directive logic

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appInputFormat]'
+  selector: '[appInputFormat]',
 })
 export class InputFormatDirective {
+  // @HostListener('focus') onfocus() {
+  //   console.log('on focus');
+  // }
 
-  constructor() { }
-
+  @Input('format')
+  format: string = '';
+  @HostListener('blur') onBlur() {
+    let value: string = this.el.nativeElement.value;
+    if (this.format == 'uppercase')
+      this.el.nativeElement.value = value.toUpperCase();
+    else this.el.nativeElement.value = value.toLowerCase();
+  }
+  constructor(private el: ElementRef) {}
 }
