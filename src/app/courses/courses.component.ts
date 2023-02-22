05320073afde330f6f222ef87a5ce445fd3e0999
samad20 <musab.aljahdhami@gmail.com>
2023-02-22 09:10:44 +0400

    ngFor exapmles

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -28,6 +28,12 @@ export class CoursesComponent {
 
     releaseDate: new Date(2016, 3, 1),
   };
+
+  coursesList = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+  ];
   onSave($event: any) {
     console.log('button was clicked', $event);
   }
@@ -36,6 +42,20 @@ export class CoursesComponent {
   }
   onFavoriteChanged() {
     console.log('Favorite changed');
+  }
+
+  onAdd() {
+    this.coursesList.push({ id: 4, name: 'course4' });
+  }
+  onRemove(course: { id: number; name: string }) {
+    let index = this.coursesList.indexOf(course);
+    this.coursesList.splice(index, 1);
+  }
+  onChange(course: { id: number; name: string }) {
+    course.name = 'Name Changed!';
+  }
+  trackCource(index: any, course: { id: number; name: string }) {
+    return course ? course.id : undefined;
   }
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
